36a9800194c7423602106ff360a6f0068cc49778
Fabian Strottmann <fabian.strottmann@gmail.com>
2025-09-17 10:48:39 +0200

    Aufgabe 4.5.3.1

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
@@ -4,6 +4,25 @@ class S4531 extends StatelessWidget {
   const S4531({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Aufg_4531();
+  }
+}
+
+class Aufg_4531 extends StatelessWidget {
+  const Aufg_4531({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 100,
+          width: 100,
+          color: Colors.blueGrey,
+        ),
+        Container(height: 100, width: 100, color: Colors.blue),
+        Container(height: 100, width: 100, color: Colors.cyan)
+      ],
+    );
   }
 }
