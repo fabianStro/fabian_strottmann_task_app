bb2d19729e8c01f7c4fe7a1bfc505337facdac77
Fabian Strottmann <fabian.strottmann@gmail.com>
2025-09-22 15:07:21 +0200

    Aufgabe 4.5.3.6

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -4,6 +4,39 @@ class S4536 extends StatelessWidget {
   const S4536({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Aufg_4536(); // Set shrinkWrap to true or false as needed
   }
+}
+
+class Aufg_4536 extends StatelessWidget {
+  Aufg_4536({super.key});
+
+  final List<Product> productDescription = [
+    Product(name: 'Bernd', price: 35.0),
+    Product(name: 'Pizza', price: 3.79),
+    Product(name: 'Brot', price: 1.99),
+    Product(name: 'Wurst', price: 2.99),
+    Product(name: 'Cola', price: 1.39)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: productDescription.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(productDescription[index].name),
+          subtitle: Text(productDescription[index].price.toString()),
+        ); // ListTile
+      }, // itemBuilder
+    ); // ListViewBuilder
+  }
+}
+
+class Product {
+  final String name;
+  final double price;
+
+  Product({required this.name, required this.price});
 }
